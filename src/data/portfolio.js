16f89d502f378c7e34161a5ac90566029ba2a0d3
Mark Howard <markhoward2005@gmail.com>
2022-12-22 15:25:19 +0000

    Fixed Images Not Loading On Build Bug

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -2,19 +2,19 @@
 export default [
     {
         title: "Note.it",
-        imgUrl: "/assets/noteit.png",
+        imgUrl: "/portfolio/assets/noteit.png",
         stack: ["Swift", "SwiftUI", "iOS", "macOS"],
         link: "https://github.com/markydoodled/Note.it"
     },
     {
         title: "DW Episode Guide",
-        imgUrl: "/assets/dwepguide.png",
+        imgUrl: "/portfolio/assets/dwepguide.png",
         stack: ["Swift", "SwiftUI", "iOS", "macOS", "tvOS", "watchOS"],
         link: "https://github.com/markydoodled/DW-Episode-Guide"
     },
     {
         title: "Monitor.it",
-        imgUrl: "/assets/monitorit.png",
+        imgUrl: "/portfolio/assets/monitorit.png",
         stack: ["Swift", "SwiftUI", "watchOS"],
         link: "https://github.com/markydoodled/Monitor.it"
     }
