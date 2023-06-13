ea8cb6f9d639da5b02dccb22e2e8b74ac82c8cbb
Mark Howard <markhoward2005@gmail.com>
2023-06-13 13:57:30 +0100

    Update Intro Text

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -8,7 +8,7 @@ function Intro() {
             {/*Intro Summary Tag Text And Styling*/}
             <p className="text-base md:text-xl mb-3 font-medium">Digital Tinkerer</p>
             {/*Intro Personal Description Summary Text And Styling*/}
-            <p className="text-sm max-w-xl mb-6 font-bold">Hi, I’m Mark. I’m Interested In Developing For Apple Systems. Currently I Am Learning Swift And SwiftUI. Soon I'll Be Looking Into Making Web Apps With ASP.NET Core And Android Apps Using Kotlin. I’m Always Looking To Collaborate On SwiftUI Projects With The Community And Am Always Happy To Help With Trying To Diagnose Bugs Or Suggest Ideas.</p>
+            <p className="text-sm max-w-xl mb-6 font-bold">Hi, I’m Mark. I’m Interested In Developing For The Apple Ecosystem. Currently I Am Learning Swift And SwiftUI. I’m Always Looking To Collaborate On SwiftUI Projects With The Community To Advance My Skills, And Am Always Happy To Help With Trying To Diagnose Bugs Or Suggest Ideas.</p>
         </div>
     )
 }
