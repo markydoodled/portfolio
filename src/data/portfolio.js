e2c04e8059b081e40fcaa6390774a408c9915ad3
Mark Howard <markhoward2005@gmail.com>
2024-05-07 21:01:43 +0000

    Added Back Image URLs Portfolio Base

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -2,73 +2,73 @@
 export default [
     {
         title: "QuickCode",
-        imgUrl: "/assets/quickcode.png",
+        imgUrl: "/portfolio/assets/quickcode.png",
         stack: ["Code Editor", "Swift", "SwiftUI", "iOS", "macOS"],
         link: "https://github.com/markydoodled/QuickCode"
     },
     {
         title: "Top Row",
-        imgUrl: "/assets/toprow.png",
+        imgUrl: "/portfolio/assets/toprow.png",
         stack: ["Menu Bar Note", "Swift", "SwiftUI", "macOS"],
         link: "https://github.com/markydoodled/Top-Row"
     },
     {
         title: "Brushr",
-        imgUrl: "/assets/brushr.png",
+        imgUrl: "/portfolio/assets/brushr.png",
         stack: ["Toothbrushing Timer", "Swift", "SwiftUI", "iOS", "watchOS"],
         link: "https://github.com/markydoodled/Brushr"
     },
     {
         title: "Next Up",
-        imgUrl: "/assets/nextup.png",
+        imgUrl: "/portfolio/assets/nextup.png",
         stack: ["Menu Bar Calendar", "Swift", "SwiftUI", "macOS"],
         link: "https://github.com/markydoodled/Next-Up"
     },
     {
         title: "Wave",
-        imgUrl: "/assets/wave.png",
+        imgUrl: "/portfolio/assets/wave.png",
         stack: ["Photo Editor", "Swift", "SwiftUI", "iOS", "macOS"],
         link: "https://github.com/markydoodled/Wave"
     },
     {
         title: "PowerWatch",
-        imgUrl: "/assets/powerwatch.png",
+        imgUrl: "/portfolio/assets/powerwatch.png",
         stack: ["Battery Monitoring", "Swift", "SwiftUI", "iOS", "watchOS"],
         link: "https://github.com/markydoodled/PowerWatch"
     },
     {
         title: "Plurality",
-        imgUrl: "/assets/plurality.png",
+        imgUrl: "/portfolio/assets/plurality.png",
         stack: ["Personality Tracker", "Swift", "SwiftUI", "iOS", "macOS", "watchOS"],
         link: "https://github.com/markydoodled/Plurality"
     },
     {
         title: "Lights Out",
-        imgUrl: "/assets/lightsout.png",
+        imgUrl: "/portfolio/assets/lightsout.png",
         stack: ["Dark Mode Web Extension", "Swift", "SwiftUI", "iOS", "macOS", "visionOS"],
         link: "https://github.com/markydoodled/Lights-Out"
     },
     {
         title: "Marking Down",
-        imgUrl: "/assets/markingdown.png",
+        imgUrl: "/portfolio/assets/markingdown.png",
         stack: ["Markdown Editor", "Swift", "SwiftUI", "iOS", "macOS"],
         link: "https://github.com/markydoodled/Marking-Down"
     },
     {
         title: "Single Track",
-        imgUrl: "/assets/singletrack.png",
+        imgUrl: "/portfolio/assets/singletrack.png",
         stack: ["Focus Timer", "Swift", "SwiftUI", "iOS", "macOS", "visionOS"],
         link: "https://github.com/markydoodled/Single-Track"
     },
     {
         title: "AllerTrack",
-        imgUrl: "/assets/allertrack.png",
+        imgUrl: "/portfolio/assets/allertrack.png",
         stack: ["Allergy Trigger Detector", "Swift", "SwiftUI", "iOS"],
         link: "https://github.com/markydoodled/AllerTrack"
     },
     {
         title: "",
-        imgUrl: "/assets/.png",
+        imgUrl: "/portfolio/assets/.png",
         stack: ["iOS"],
         link: "https://github.com/markydoodled/"
     }
