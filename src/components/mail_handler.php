f6b23702119815a8928553e0fc0bccc9cd23e2f2
Mark Howard <markhoward2005@gmail.com>
2024-05-07 19:47:06 +0000

    Updated Mail Hander Info

--- src/components/mail_handler.php
+++ src/components/mail_handler.php
@@ -4,18 +4,18 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST['email'];
     $message = $_POST['message'];
 
-    $to = 'your-email@example.com';
-    $subject = 'New Contact Form Submission';
+    $to = 'markhoward@markydoodled.com';
+    $subject = 'New Portfolio Form Submission';
     $headers = "From: $email" . "\r\n" .
                "Reply-To: $email" . "\r\n" .
                'X-Mailer: PHP/' . phpversion();
 
-    $body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email\n\nMessage:\n$message";
+    $body = "You have received a new submission from your portfolio contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email\n\nMessage:\n$message";
 
     if(mail($to, $subject, $body, $headers)) {
-        echo 'Message sent successfully!';
+        echo 'Message Sent Successfully!';
     } else {
-        echo 'Message could not be sent.';
+        echo 'Message Could Not Be Sent.';
     }
 }
 ?>
