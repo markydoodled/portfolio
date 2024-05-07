8e299a9fee0a31bbe631451d8452340ca519b9ad
Mark Howard <markhoward2005@gmail.com>
2024-05-07 12:30:06 +0000

    Added Extra Portfolio Items

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -17,5 +17,59 @@ export default [
         imgUrl: "/portfolio/assets/brushr.png",
         stack: ["Toothbrushing Timer", "Swift", "SwiftUI", "iOS", "watchOS"],
         link: "https://github.com/markydoodled/Brushr"
+    },
+    {
+        title: "Next Up",
+        imgUrl: "/portfolio/assets/nextup.png",
+        stack: ["Menu Bar Calendar", "Swift", "SwiftUI", "macOS"],
+        link: "https://github.com/markydoodled/Next-Up"
+    },
+    {
+        title: "",
+        imgUrl: "/portfolio/assets/.png",
+        stack: ["iOS"],
+        link: "https://github.com/markydoodled/"
+    },
+    {
+        title: "",
+        imgUrl: "/portfolio/assets/.png",
+        stack: ["iOS"],
+        link: "https://github.com/markydoodled/"
+    },
+    {
+        title: "",
+        imgUrl: "/portfolio/assets/.png",
+        stack: ["iOS"],
+        link: "https://github.com/markydoodled/"
+    },
+    {
+        title: "",
+        imgUrl: "/portfolio/assets/.png",
+        stack: ["iOS"],
+        link: "https://github.com/markydoodled/"
+    },
+    {
+        title: "",
+        imgUrl: "/portfolio/assets/.png",
+        stack: ["iOS"],
+        link: "https://github.com/markydoodled/"
+    },
+    {
+        title: "",
+        imgUrl: "/portfolio/assets/.png",
+        stack: ["iOS"],
+        link: "https://github.com/markydoodled/"
+    },
+    {
+        title: "",
+        imgUrl: "/portfolio/assets/.png",
+        stack: ["iOS"],
+        link: "https://github.com/markydoodled/"
+    },
+    {
+        title: "",
+        imgUrl: "/portfolio/assets/.png",
+        stack: ["iOS"],
+        link: "https://github.com/markydoodled/"
     }
 ]
