e576813c37c307507a564235c7b68111251cd7e9
Mark Howard <markhoward2005@gmail.com>
2024-05-19 14:14:31 +0000

    Finished Feedback Mail Page Styling And Functionality

--- public/mail_handler.php
+++ public/mail_handler.php
@@ -23,10 +23,98 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
 }
 
 if(isset($_GET['message']) && $_GET['message'] == 'Email Sent Successfully') {
-    echo '<p style="color: green; font-size: 20px;">' . $_GET['message'] . '</p>';
+    echo '
+    <style>
+        .message-container {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+        }
+        .message-container p {
+            font-size: 2rem;
+            margin-bottom: 0.25rem;
+            font-weight: 700;
+            justify-content: center;
+            align-items: center;
+        }
+
+        @media (min-width: 768px) {
+            .message-container p {
+                font-size: 2rem;
+                margin-bottom: 0.75rem;
+                justify-content: center;
+                align-items: center;
+            }
+        }
+        .message-container button {
+            display: flex;
+            align-items: center;
+            padding: 10px 20px;
+            font-size: 1em;
+            font-weight: bold;
+            background: linear-gradient(to right, orange, purple);
+            color: white;
+            border-radius: 5px;
+            text-decoration: none;
+            margin-top: 1em;
+        }
+    </style>
+    <div class="message-container">
+        <p>' . $_GET['message'] . '</p>
+        <a href="index.html">
+            <button>Go Back Home</button>
+        </a>
+    </div>';
 }
 
-/*if(isset($_GET['message'])) {
-    $message = $_GET['message'];
-}*/
+if(isset($_GET['message']) && $_GET['message'] == 'Email Could Not Be Sent') {
+    echo '
+    <style>
+        .message-container {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+        }
+        .message-container p {
+            font-size: 2rem;
+            margin-bottom: 0.25rem;
+            font-weight: 700;
+            justify-content: center;
+            align-items: center;
+            color: red;
+        }
+
+        @media (min-width: 768px) {
+            .message-container p {
+                font-size: 2rem;
+                margin-bottom: 0.75rem;
+                justify-content: center;
+                align-items: center;
+                color: red;
+            }
+        }
+        .message-container button {
+            display: flex;
+            align-items: center;
+            padding: 10px 20px;
+            font-size: 1em;
+            font-weight: bold;
+            background: linear-gradient(to right, orange, purple);
+            color: white;
+            border-radius: 5px;
+            text-decoration: none;
+            margin-top: 1em;
+        }
+    </style>
+    <div class="message-container">
+        <p>' . $_GET['message'] . '</p>
+        <a href="index.html">
+            <button>Go Back Home</button>
+        </a>
+    </div>';
+}
 ?>
