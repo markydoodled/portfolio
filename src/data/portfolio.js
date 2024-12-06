f238eec8598a72d641845595c868b60fdfbb4b84
Mark Howard <markhoward2005@gmail.com>
2024-12-06 19:35:11 +0000

    Swapped Portfolio Items Around

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -13,57 +13,15 @@ export default [
         link: "https://github.com/markydoodled/Top-Row"
     },
     {
-        title: "Brushr",
-        imgUrl: "/assets/brushr.png",
-        stack: ["Toothbrushing Timer", "Swift", "SwiftUI", "iOS", "watchOS"],
-        link: "https://github.com/markydoodled/Brushr"
-    },
-    {
         title: "Next Up",
         imgUrl: "/assets/nextup.png",
         stack: ["Menu Bar Calendar", "Swift", "SwiftUI", "macOS"],
         link: "https://github.com/markydoodled/Next-Up"
-    }/*,
-    {
-        title: "Wave",
-        imgUrl: "/assets/wave.png",
-        stack: ["Photo Editor", "Swift", "SwiftUI", "iOS", "macOS"],
-        link: "https://github.com/markydoodled/Wave"
     },
     {
-        title: "PowerWatch",
-        imgUrl: "/assets/powerwatch.png",
-        stack: ["Battery Monitoring", "Swift", "SwiftUI", "iOS", "watchOS"],
-        link: "https://github.com/markydoodled/PowerWatch"
-    },
-    {
-        title: "Plurality",
-        imgUrl: "/assets/plurality.png",
-        stack: ["Personality Tracker", "Swift", "SwiftUI", "iOS", "macOS", "watchOS"],
-        link: "https://github.com/markydoodled/Plurality"
-    },
-    {
-        title: "Lights Out",
-        imgUrl: "/assets/lightsout.png",
-        stack: ["Dark Mode Web Extension", "Swift", "SwiftUI", "iOS", "macOS", "visionOS"],
-        link: "https://github.com/markydoodled/Lights-Out"
-    },
-    {
-        title: "Marking Down",
-        imgUrl: "/assets/markingdown.png",
-        stack: ["Markdown Editor", "Swift", "SwiftUI", "iOS", "macOS"],
-        link: "https://github.com/markydoodled/Marking-Down"
-    },
-    {
-        title: "Single Track",
-        imgUrl: "/assets/singletrack.png",
-        stack: ["Focus Timer", "Swift", "SwiftUI", "iOS", "macOS", "visionOS"],
-        link: "https://github.com/markydoodled/Single-Track"
-    },
-    {
-        title: "AllerTrack",
-        imgUrl: "/assets/allertrack.png",
-        stack: ["Allergy Trigger Detector", "Swift", "SwiftUI", "iOS"],
-        link: "https://github.com/markydoodled/AllerTrack"
-    }*/
+        title: "Brushr",
+        imgUrl: "/assets/brushr.png",
+        stack: ["Toothbrushing Timer", "Swift", "SwiftUI", "iOS", "watchOS"],
+        link: "https://github.com/markydoodled/Brushr"
+    }
 ]
